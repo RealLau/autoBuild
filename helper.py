863961a1785dee1922d1c726390ed0c2ea7dfc1c
RealLau <921363139@qq.com>
2017-09-06 09:42:01 +0800

    Update helper.py

--- helper.py
+++ helper.py
@@ -24,7 +24,7 @@ class Helper:
         i = r.info()
         svnLatestVersion = i["entry#revision"]
         print(svnLatestVersion)
-        return "svnlatest"+str(svnLatestVersion)
+        return "_"+str(svnLatestVersion)
     
     @classmethod
     def getGradleAppVersion(self, localDir):
@@ -36,7 +36,7 @@ class Helper:
             if resultGradleInfo:
                 firstResultGradleInfo = resultGradleInfo[0]
                 print(firstResultGradleInfo)
-                return firstResultGradleInfo.split(" ")[1].replace('"', "")
+                return firstResultGradleInfo.split(" ")[1].replace('"', "")+"_"
     
     @classmethod        
     def buildApk(self, localDir, apkGenerateDir, release = False):
@@ -78,10 +78,11 @@ class Helper:
         localClie.commit("automation commit test")
     
     @classmethod
-    def sendEmail(self, toList, fullContent, emailserver, emailuserName, emailpwd): 
+    def sendEmail(self, toList, fullContent, emailserver, emailuserName, emailpwd, cc=None): 
         msg=email.mime.multipart.MIMEMultipart()  
         msg['from']=emailuserName  
-        msg['to']=toList 
+        msg['to']=toList
+        msg['cc'] = cc
         msg['subject']='test'  
         txt=email.mime.text.MIMEText(fullContent)  
         msg.attach(txt)   
@@ -89,7 +90,7 @@ class Helper:
         smtp=smtplib.SMTP()  
         smtp.connect(emailserver,25)  
         smtp.login(emailuserName,emailpwd)  
-        smtp.sendmail(from_addr=emailuserName, to_addrs=toList.split(",") , msg=msg.as_string())
+        smtp.sendmail(from_addr=emailuserName, to_addrs=toList.split(",")+([] if cc==None else msg['cc'].split(",")) , msg=msg.as_string())
         smtp.quit()  
     
     @classmethod
@@ -107,61 +108,76 @@ class Helper:
         with open("conf.ini", "w") as conf:
             config.write(conf)
             conf.close()
-            
+# Helper.buildApk(localDir = r"C:\SVNAndroidSourceCode3.1", apkGenerateDir=r"C:\SVNAndroidSourceCode3.1\cyb\build\outputs\apk")
+# Helper.sendEmail( toList="liushuangshuang@hongfans.cn", fullContent="HHHHHHHH", emailserver="smtp.exmail.qq.com", emailuserName="liushuangshuang@hongfans.cn", emailpwd="Shuang142693")
+# Helper.copyApkAndRename(sourceFileFullPath=os.path.join(r"C:\SVNAndroidSourceCode3.1", "cyb","build","outputs","apk","cyb-release.apk"), release=True, localSvnApkDir=r"C:\SVNProject\testForAutomation", localDir=r"C:\SVNAndroidSourceCode3.1", remoteDir="https://192.168.1.10/svn/rearview3.1/Rearview3.1/trunk")
+# Helper.readAndSetPreSettings()
 def runTask():
     pars = sys.argv[1:]
-    release, sourceCodeRemoteField, localDir, apkGenerateDir, localSvnApkDir, apkRemoteField, emailServerField,emailUserNameField,emailPwdField,emailReceiversField,emailPrefixField, updadeSourceCode, buildApk, submitApk, sendEmail = pars
-    currentTime = time.strftime('%I:%M:%S %p',time.localtime(time.time()))
+    print(pars)
+    release, sourceCodeRemoteField, localDir, apkGenerateDir, localSvnApkDir, apkRemoteField, emailServerField,emailUserNameField,emailPwdField,emailReceiversField,emailCCField, updadeSourceCode, buildApk, submitApk, sendEmail = pars
+    
+
+    with open("emailPrefix.txt", "r") as f:
+        emailPrefixField = f.read()
+#     currentTime = time.strftime('%I:%M:%S %p',time.localtime(time.time()))
     fileName = None
     if updadeSourceCode=="True":
         try:
             print("=======step 1: 正在使用svn更新apk源码...=======")
             Helper.updateSourceCode(localDir=localDir)
             print("=======step 1: 使用svn更新apk源码成功=======\n\n")
-        except Exception:
-            print("=======step 1: 更新apk源码失败=======")
+        except Exception as e:
+            print("=======step 1: 更新apk源码失败=======\n")
+            print(str(e))
             return
-        print(buildApk)
-        if buildApk=="True":
-            try:
-                print("=======step 2: Gradle编译生成apk...=======")
-                Helper.buildApk(localDir=localDir, apkGenerateDir=apkGenerateDir, release=release)
-                print("=======step 2: Gradle编译生成apk成功=======\n\n")
-            except Exception:
-                print("=======step 2: Gradle编译生成apk失败=======")
-                return
-        if submitApk=="True":
-            try:
-                print("=======step 3: 正在复制生成的apk到svn apk文件存放目录...=======")
-                lastFix = None
-                if release:
-                    lastFix = "cyb-release.apk"
-                else:
-                    lastFix = "cyb-debug.apk"
-                sourceFileFullPath = os.path.join(localDir, "cyb","build","outputs","apk",lastFix)
-                fileName = Helper.copyApkAndRename(sourceFileFullPath=sourceFileFullPath, release=release, localSvnApkDir=localSvnApkDir, localDir=localDir, remoteDir=sourceCodeRemoteField)
-                print("=======step 3: 复制生成的apk到svn apk文件存放目录成功=======\n\n")
-            except Exception:
-                print("=======step 3: 复制生成的apk到svn apk文件存放目录失败=======")
-                return
-            try:
-                print("=======step 4: svn提交生成apk...=======")
-                Helper.addAndCommitApk(localSvnApkDir=localSvnApkDir, fileName=fileName)
-                print("=======step 4: svn提交生成apk成功=======\n\n")
-            except Exception:
-                print("=======step 4: svn提交生成apk失败=======")
-                return    
-        if sendEmail=="True":         
-            try:
-                print("=======step 5: 邮件发送中...=======")
-                fullContent = emailPrefixField+":\n"+apkRemoteField+fileName
-                toList = emailReceiversField
+    if buildApk=="True":
+        try:
+            print("=======step 2: Gradle编译生成apk...=======")
+            Helper.buildApk(localDir=localDir, apkGenerateDir=apkGenerateDir, release=release)
+            print("=======step 2: Gradle编译生成apk成功=======\n\n")
+        except Exception as e:
+            print("=======step 2: Gradle编译生成apk失败=======\n")
+            print(str(e))
+            return
+    if submitApk=="True":
+        try:
+            print("=======step 3: 正在复制生成的apk到svn apk文件存放目录...=======")
+            lastFix = None
+            if release:
+                lastFix = "cyb-release.apk"
+            else:
+                lastFix = "cyb-debug.apk"
+            sourceFileFullPath = os.path.join(localDir, "cyb","build","outputs","apk",lastFix)
+            fileName = Helper.copyApkAndRename(sourceFileFullPath=sourceFileFullPath, release=release, localSvnApkDir=localSvnApkDir, localDir=localDir, remoteDir=sourceCodeRemoteField)
+            print("=======step 3: 复制生成的apk到svn apk文件存放目录成功=======\n\n")
+        except Exception as e:
+            print("=======step 3: 复制生成的apk到svn apk文件存放目录失败=======\n")
+            print(str(e))
+            return
+        try:
+            print("=======step 4: svn提交生成apk...=======")
+            Helper.addAndCommitApk(localSvnApkDir=localSvnApkDir, fileName=fileName)
+            print("=======step 4: svn提交生成apk成功=======\n\n")
+        except Exception as e:
+            print("=======step 4: svn提交生成apk失败=======\n")
+            print(str(e))
+            return    
+    if sendEmail=="True":         
+        try:
+            print("=======step 5: 邮件发送中...=======")
+            fullContent = emailPrefixField+apkRemoteField+fileName
+            toList = emailReceiversField
+            if emailCCField!="NONE":
+                Helper.sendEmail(toList, fullContent, emailserver=emailServerField, emailuserName=emailUserNameField, emailpwd=emailPwdField, cc=emailCCField)
+            else:
                 Helper.sendEmail(toList, fullContent, emailserver=emailServerField, emailuserName=emailUserNameField, emailpwd=emailPwdField)
-                print("=======step 5: 邮件发送成功=======\n\n")
-            except Exception:
-                print("=======step 5: 邮件发送失败=======")
-                return
-        print("\n\n☆☆☆☆☆☆当前任务执行完毕☆☆☆☆☆☆☆")
+            print("=======step 5: 邮件发送成功=======\n\n")
+        except Exception as e:
+            print("=======step 5: 邮件发送失败=======")
+            print(str(e))
+            return
+    print("\n\n☆☆☆☆☆☆当前任务执行完毕☆☆☆☆☆☆☆")
         
 if __name__=="__main__":
     runTask()
